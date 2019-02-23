4910676d6108af66d7b1d45257c2c16ea0fa1b95
Lauren Hilton <lauren.hilton@daugherty.com>
2019-02-22 21:46:34 -0600

    base project

---
+++ AwesomeProject/AwesomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeProject
+//
+//  Created by Lauren Hilton on 2/22/19.
+//  Copyright © 2019 DaughertyGLHTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
